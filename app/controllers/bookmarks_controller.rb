e2016dbcbec4f8c2a8101639c0ae492a83d86328
Simon Herout <simon@herout.eu>
2021-08-12 16:02:12 +0200

    Il manque le CSS

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,47 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: [:create, :new]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list), notice: 'Bookmark was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @bookmark.update(bookmark_params)
+      redirect_to @bookmark, notice: 'Bookmark was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_url(@bookmark.list), notice: 'Bookmark was successfully destroyed.'
+  end
+
+  private
+
+  def set_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+  end
+end
